a807c5c3d095c7090033014133a5effe9482f9f0
Gavin Pierce <gavin.m.pierce@maine.edu>
2025-11-10 03:49:27 +0000

    server.js changes mainly

--- node-js/server.js
+++ node-js/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const session = require('express-session');
-const { requireLogin } = require("./auth");
+const { requireLogin } = require("./auth"); // make sure someone is logged in to access page
 const app = express();
 const path = require('path');
 const hbs = require('hbs');
+const cookieParser = require('cookie-parser');
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+
+// parses form submissions
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser()); 
 
 
 const PORT = 8080;
@@ -17,17 +22,50 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: false, // Set to true if using HTTPS
+        secure: false, 
         maxAge: 24 * 60 * 60 * 1000 // 24 hours
     }
 }));
 
 app.get('/',(req,res) => {
-    res.render('main');
+    let user = {
+        username: "Guest",
+        isLoggedIn: false,
+        password: ""
+    }
+    if (req.session.isLoggedIn) {
+        user = {
+            name: req.session.username,
+            isLoggedIn: true,
+            loginTime: req.session.loginTime,
+            visitCount: req.session.visitCount || 0
+        };
+        
+        // Increment visit count
+        req.session.visitCount = (req.session.visitCount || 0) + 1;
+    }
+    res.render('main',{ user: user });
 });
 
 app.get('/register', (req,res) => {
     res.render('register');
+})
+
+app.post('/register',(req,res) => {
+    const name = (req.body && req.body.name) ? req.body.name : '';
+    
+    // Set cookie with user data
+    res.cookie('name', JSON.stringify({
+        name: name, 
+        msg: "Hello there!"
+    }), { 
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000, // 24 hours
+        secure: false,
+        sameSite: "lax"
+    });
+    
+    res.redirect('/login');
 })
 
 
@@ -35,28 +73,53 @@ app.get('/login',(req,res) => {
     res.render('login');
 });
 
+app.post('/login',(req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    // Simple authentication (in production, use proper password hashing)
+    if (username && password) {
+        // Set session data
+        req.session.isLoggedIn = true;
+        req.session.username = username;
+        req.session.loginTime = new Date().toISOString();
+        
+        console.log(`User ${username} logged in at ${req.session.loginTime}`);
+        res.redirect('/');
+    } else {
+        res.redirect('/login?error=1');
+    }
+    
+})
+
 app.get('/comments',(req,res) => {
-    res.render('comments');
+
+    let comments = {
+        title: "Guest",
+        content: "",
+        author: "",
+        date: ""
+    }
+
+    res.render('comments',{ user: user }, {comments:comments});
 })
 
 app.get('/comment/new',requireLogin,(req,res) => {
-    res.render('newcomment');
+    res.render('newcomment',{ user: user });
 })
 
-app.get('/register',(req,res) => {
+app.post('/comment/new',requireLogin,(req,res) => {
     
 })
 
-app.get('/login',(req,res) => {
-    
-})
 
-app.get('/logout',requireLogin,(req,res) => {
-    
-})
-
-app.get('/comment',requireLogin,(req,res) => {
-    
+app.post('/logout',requireLogin,(req,res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log('Error destroying session:', err);
+        }
+        res.redirect('/');
+    });
 })
 
 
